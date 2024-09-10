b55958b24dcbb812b0a774d54b5e1009b0dc63e0
MightyGarlic <161018379+MightyGarlic@users.noreply.github.com>
2024-09-10 18:22:38 +0200

    Param editorsChoice works now, added link to the unofficial python pixabay library

--- ImageFetcher.py
+++ ImageFetcher.py
@@ -11,19 +11,21 @@ os.makedirs(log_directory, exist_ok=True)
 log_file = os.path.join(log_directory, 'ImageFetcher.log')
 logging.basicConfig(filename=log_file,encoding='utf-8',level=logging.INFO, filemode = 'w', format='%(levelname)s: %(message)s')
 
-def get_images_from_pixabay(API_key: str, keyword: str, num_images: Optional[int] = None, lang: Optional[str] = 'en', image_type: Optional[str] = 'all',
-                            editors_choice: Optional[bool] = False, image_size: Optional[str] = 'webformat') -> None:
+def get_images_from_pixabay(API_key: str, keyword: str, num_images: Optional[int] = None, lang: Optional[str] = 'en',
+                            editorsChoice: Optional[int] = False,
+                            image_size: Optional[str] = 'webformat') -> None:
     
     ''' 
-    Full Pixabay API documentation: https://pixabay.com/api/docs/
-
-    ----------------
-
     image_type: str, optional
         Filter results by image type. Accepted values: 'all', 'photo", 'illustration', 'vector' 
 
     image_size : str, optional 
         Size of the image to download ('preview', 'webformat', 'largeImage'). Default is 'webformat'
+    
+    ----------------
+    Full Pixabay API documentation: https://pixabay.com/api/docs/
+    ----------------
+    Python wrapper documentation: https://github.com/Lukas0025/python-pixabay/tree/main
 
     '''
     
@@ -31,7 +33,7 @@ def get_images_from_pixabay(API_key: str, keyword: str, num_images: Optional[int
     px = pixabay.core(API_key)
 
     # search for keyword
-    media = px.query(keyword, lang, image_type, editors_choice)
+    media = px.query(query=keyword, lang=lang, perPage=200, editorsChoice=editorsChoice)
 
     # amount of total hits
     hits = len(media)
